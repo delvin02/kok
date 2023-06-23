2354f976af115903af2d5cfd7152f7ad6a8a6e41
delvin02 <weeiwaai02@gmail.com>
2023-06-23 01:18:00 +0000

    wsgi

--- kck/wsgi.py
+++ kck/wsgi.py
@@ -1,7 +1,8 @@
-import os
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/var/www/html/kck/venv/lib/python3.6/site-packages')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kck.settings')
 
 application = get_wsgi_application()
